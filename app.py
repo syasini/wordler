0bcf8855cf1aef3c3641df7c84cc7f24297e940a
Siavash Yasini <siavash.yasini@gmail.com>
2022-02-23 20:59:21 -0800

    Update header and info.

--- app.py
+++ app.py
@@ -2,21 +2,22 @@
 
 import streamlit as st
 
-from wordler.utils import submit_guesses, print_letters_hints, is_valid_word, keep_valid_guesses, convert_guess_and_hints, get_logo
+from wordler.utils import submit_guesses, print_letters_hints, is_valid_word, keep_valid_guesses, convert_guess_and_hints, get_logo, parse_metric
 from wordler.solver import Wordler
 
 get_logo()
 
 st.sidebar.title("WORDLEr")
 st.sidebar.caption("A [WORDLE](https://www.nytimes.com/games/wordle/index.html) ch🟨🟩t sh🟨🟨t made by [Siavash Yasini](https://www.linkedin.com/in/siavash-yasini/).")
-st.markdown("""
-1. Type in the 5️⃣ letters for your GUESS in each step (Start with **_ARISE_**! Trust me...). 
-2. Select the colors you get back from [WORDLE](https://www.nytimes.com/games/wordle/index.html) for each letter, 
-3. Press 'submit' to get the top suggestions for next guess.
-4. Use the 👈 sidebar to change the settings.
-5. Share this app with your friends! 
-_Tip_: This app is meant to be used on a big screen. If you are on a small screen turn it sideways for a better experience.
-""")
+with st.expander("How to take the fun out of WORDLE", expanded=True):
+    st.markdown("""
+    1. Type in the 5️⃣ letters for your GUESS in each step (Start with **_ARISE_**! Trust me...). 
+    2. Select the colors you get back from [WORDLE](https://www.nytimes.com/games/wordle/index.html) for each letter, 
+    3. Press 'submit' to get the top suggestions for next guess.
+    4. Use the 👈 sidebar to change the settings.
+    5. Share this app with your friends! 
+    _Tip_: This app is meant to be used on a big screen. If you are on a small screen turn it sideways for a better experience.
+    """)
 
 st.sidebar.header("Settings")
 
@@ -51,5 +52,8 @@ Note on metrics:\n
 Words with more frequent letters have a higher '**letter score**' (suggested for 1st or 2nd guesses).\n
 
 More frequent words have a higher '**wiki score**' (suggested for later guesses). 
+
+The metric '**auto**' will automatically use '**letter score**' for the first and second suggestions, and then '**wiki score**' for the rest.
+The idea here is to maximize the chances of discovering all the letters in the first two guesses, and then aiming to find the target through the most common words.
 
 """)
